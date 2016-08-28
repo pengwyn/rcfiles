95f48f2a66bc25e91725bbb4e94437386c50be90
Daniel Cocks <daniel.cocks@gmail.com>
2016-08-29 00:02:28 +1000

    Stack of emacs changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,7 +26,8 @@
     evil-exchange
     evil-search-highlight-persist
     magit
-    relative-line-numbers))
+    relative-line-numbers
+    helm))
 
 
 (mapc #'(lambda (package)
@@ -35,7 +36,7 @@
       myPackages)
 
 (elpy-enable)
-;(elpy-use-ipython)
+(delete 'elpy-module-highlight-indentation elpy-modules)
 (setq elpy-rpc-python-command "python2")
 (elpy-use-ipython "ipython2")
 
@@ -45,15 +46,12 @@
 ;; --------------------------------------
 
 (setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
+(load-theme 'alect-black t) ;; load material theme
 ;(global-linum-mode t) ;; enable line numbers globally
 (global-relative-line-numbers-mode)
 
-;; init.el ends here
-
-;; Danny stuff
-;(autoload 'python-mode "python-mode.el" "Python mode." t)
-;(setq auto-mode-alist (append '(("/.*\.py\'" . python-mode)) auto-mode-alist))
+(add-to-list 'default-frame-alist '(font . "GohuFont-11"))
+(set-face-attribute 'default t :font "GohuFont-11")
 
 (setq python-shell-interpreter "ipython2"
     python-shell-interpreter-args "--simple-prompt -i")
@@ -109,9 +107,113 @@ With negative prefix, apply to -N lines above."
 (define-key evil-normal-state-map (kbd "C-w C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-w C-j") 'evil-window-down)
 
+(define-key evil-insert-state-map (kbd "C-a") 'evil-beginning-of-line)
+(define-key evil-insert-state-map (kbd "C-e") 'evil-end-of-line)
+
+(define-key evil-visual-state-map ">" (lambda ()
+    (interactive)
+    ; ensure mark is less than point
+    (when (> (mark) (point)) 
+        (exchange-point-and-mark)
+    )
+    (evil-normal-state)
+    (evil-shift-right (mark) (point))
+    (evil-visual-restore) ; re-select last visual-mode selection
+))
+
+(define-key evil-visual-state-map "<" (lambda ()
+    (interactive)
+    ; ensure mark is less than point
+    (when (> (mark) (point)) 
+        (exchange-point-and-mark)
+    )
+    (evil-normal-state)
+    (evil-shift-left (mark) (point))
+    (evil-visual-restore) ; re-select last visual-mode selection
+))
+
 (global-auto-revert-mode t)
 
 (setq-default evil-symbol-word-search 'symbol)
 
 (with-eval-after-load 'evil
     (defalias #'forward-evil-word #'forward-evil-symbol))
+
+(setq-default company-minimum-prefix-length 2)
+(define-prefix-command 'danny-completions)
+(define-key evil-insert-state-map (kbd "C-k") 'danny-completions)
+(define-key evil-insert-state-map (kbd "C-k C-l") 'evil-complete-previous-line)
+
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(require 'semantic)
+(require 'semantic/db-global)
+(semanticdb-enable-gnu-global-databases 'c-mode)
+(semanticdb-enable-gnu-global-databases 'c++-mode)
+(semantic-mode 1)
+
+
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
+(global-set-key (kbd "C-x b") 'helm-mini)
+(setq helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match    t)
+(define-key evil-normal-state-map (kbd "M-o") 'helm-recentf)
+
+(require 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1))))
+
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+;(define-key evil-normal-state-map (kbd "M-.") 'ggtags-find-tag-dwim)
+(define-key evil-normal-state-map (kbd "M-.") nil)
+
+(setq-default c-default-style "linux"
+              c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode t)
+
+;(add-to-list
+; 'display-buffer-alist
+; '("\\*compilation\\*" . (display-buffer-pop-up-window)))
+
+(setq-default split-height-threshold 20
+			  split-width-threshold 20)
+
+(require 'powerline)
+(powerline-center-evil-theme)
