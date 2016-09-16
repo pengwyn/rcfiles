03a7eb974825e4539b61524859222aed42c6a0cc
Daniel Cocks <daniel.cocks@gmail.com>
2016-09-16 15:32:34 +1000

    Safe variable names

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -345,3 +345,17 @@ With negative prefix, apply to -N lines above."
     (define-key comint-mode-map (kbd "C-p") 'comint-previous-input)
     (define-key comint-mode-map (kbd "<down>") 'comint-next-input)
     (define-key comint-mode-map (kbd "C-n") 'comint-previous-input)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+	((company-clang-arguments "-I/home/pengwyn/work4/ccode/SwarmMC/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
