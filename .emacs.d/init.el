e9767871b6e880f7447aae0ac0153b97e16ceb84
Daniel Cocks <daniel.cocks@gmail.com>
2016-12-28 09:50:36 +1000

    Fixed up prettification

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,6 +121,32 @@ With negative prefix, apply to -N lines above."
 
 (setq-default recentf-max-saved-items 1000)
 
+(require 'prettify-greek)
+
+; Make up my own set with \ in front of them.
+(defconst danny-prettify-set 
+	(let* ((my-greek (append (copy-tree prettify-greek-lower) (copy-tree prettify-greek-upper))))
+	  (dolist (item my-greek)
+		(setcar item (concat "\\" (car item))))
+	  (append my-greek prettify-greek-lower prettify-greek-upper)))
+
+(defun danny-prettify-predicate (start end _match) "Only care about words and not symbols."
+	   ;; (not (or (= (char-syntax (char-after end)) ?w)
+	   ;; 			(= (char-syntax (char-before start)) ?w))))
+	   (not (or (string-match-p "[a-zA-Z]" (string (char-after end)))
+				(string-match-p "[a-zA-Z]" (string (char-before start))))))
+	   
+
+(defun danny-add-prettify-greek (mode) "Add prettify-greek symbols to mode."
+	   (add-hook mode (lambda ()
+						(setq
+							;prettify-symbols-alist (append prettify-symbols-alist prettify-greek-lower prettify-greek-upper)))))
+							prettify-symbols-alist (append prettify-symbols-alist danny-prettify-set))
+						(prettify-symbols-mode t)
+						(setq prettify-symbols-compose-predicate 'danny-prettify-predicate))))
+
+(setq-default prettify-symbols-unprettify-at-point "right-edge")
+
 ;;;;;;;;;;;;;;;
 ;; Evil stuff
 
@@ -459,6 +485,8 @@ With negative prefix, apply to -N lines above."
 
 (eval-after-load "org" '(setq-default org-modules (append org-modules '(org-habit org-mouse))))
 
+(danny-add-prettify-greek 'org-mode-hook)
+
 
 (defun danny-open-orgfile
 	(&optional arg)
@@ -494,7 +522,7 @@ With negative prefix, apply to -N lines above."
 		  ;"- [ ] %i%?\n\t%u"
 		  )))
 
-(add-hook 'org-mode 'auto-fill-mode)
+(add-hook 'org-mode-hook 'auto-fill-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Latex stuff
