9d0de9e91101f12ebd983d7cf43de7d8f416f3ed
Daniel Cocks <daniel.cocks@gmail.com>
2019-02-03 22:48:01 +1100

    Put packages in the load path

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -4,9 +4,11 @@ import Distributed
 if true
 	if Distributed.nprocs() == 1
 		pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia")
+		pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia/packages")
 		pushfirst!(LOAD_PATH, ".")
 	else
 		@eval Distributed.@everywhere pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia")
+		@eval Distributed.@everywhere pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia/packages")
 		@eval Distributed.@everywhere pushfirst!(LOAD_PATH, ".")
 	end
 
