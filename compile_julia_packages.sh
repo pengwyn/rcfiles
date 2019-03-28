cc1c430601e68eb6e230b3fa876e366bc1fe10b4
Daniel Cocks <daniel.cocks@gmail.com>
2019-03-28 11:07:04 +1100

    Less packages

--- compile_julia_packages.sh
+++ compile_julia_packages.sh
@@ -1,2 +1,3 @@
-/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck, :PyCall, :PyPlot)'
+#/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck, :PyCall, :PyPlot)'
+/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck)'
 #julia --startup-file=no -e 'using PackageCompiler; compile_package(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck)'
