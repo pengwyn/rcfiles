c3ee157e59119cb1a48e7c8121b9838c3d71346d
Daniel Cocks <daniel.cocks@gmail.com>
2019-05-24 19:52:32 +1000

    Stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1185,6 +1185,21 @@ you want to quit windows on all frames."
   (setq org-modules (append org-modules '(org-habit org-mouse)))
 
   (danny-add-prettify-greek 'org-mode-hook)
+  (add-hook 'org-mode-hook (lambda ()
+                             "Beautify Org Checkbox Symbol"
+                             (push '("[ ]" .  "☐") prettify-symbols-alist)
+                             (push '("[X]" . "☑" ) prettify-symbols-alist)
+                             (push '("[-]" . "❍" ) prettify-symbols-alist)
+                             (prettify-symbols-mode)))
+
+  (defface org-checkbox-done-text
+    '((t (:foreground "#71696A" :strike-through t)))
+    "Face for the text part of a checked org-mode checkbox.")
+  (font-lock-add-keywords
+   'org-mode
+   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
+      1 'org-checkbox-done-text prepend))
+   'append)
 
   (require 'ox-publish)
   (require 'ox-md)
@@ -1562,6 +1577,7 @@ you want to quit windows on all frames."
 (define-key danny-completions (kbd "C-h") 'helm-navi-headings)
 (define-key danny-completions (kbd "C-c") 'company-complete)
 (define-key danny-completions (kbd "C-f") 'company-files)
+(define-key danny-completions (kbd "q") 'evil-record-macro)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
