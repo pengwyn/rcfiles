267c8d8c967b5e07e2b8ffe3089d2566521e3e27
Daniel Cocks <daniel.cocks@gmail.com>
2019-07-31 01:22:47 +1000

    Removed julia_packages directory

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -2,11 +2,11 @@ import Distributed
 
 if Distributed.nprocs() == 1
 	pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia")
-	pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia_packages")
+	#pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia_packages")
 	pushfirst!(LOAD_PATH, ".")
 else
 	@eval Distributed.@everywhere pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia")
-	@eval Distributed.@everywhere pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia_packages")
+	#@eval Distributed.@everywhere pushfirst!(LOAD_PATH, "/home/pengwyn/work5/julia_packages")
 	@eval Distributed.@everywhere pushfirst!(LOAD_PATH, ".")
 end
 
