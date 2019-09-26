5f0255f61dab5d8fd464034293d452c14a618214
Daniel Cocks <daniel.cocks@gmail.com>
2019-09-26 17:20:33 +1000

    Ranger permissions and time

--- .config/ranger/plugins/devicons_linemode.py
+++ .config/ranger/plugins/devicons_linemode.py
@@ -1,5 +1,6 @@
 import ranger.api
-from ranger.core.linemode import LinemodeBase
+from ranger.core.linemode import LinemodeBase, SizeMtimeLinemode, PermissionsLinemode
+from ranger.core.linemode import LinemodeBase, SizeMtimeLinemode
 from .devicons import *
 
 @ranger.api.register_linemode
@@ -17,3 +18,28 @@ class DevIconsLinemodeFile(LinemodeBase):
 
   def filetitle(self, file, metadata):
     return devicon(file) + ' ' + file.relative_path
+  
+
+@ranger.api.register_linemode
+class DannyTime(LinemodeBase):
+  name = "danny_time"
+
+  def filetitle(self, file, metadata):
+    temp = devicon(file) + ' ' + file.relative_path
+    return temp
+  
+  def infostring(self, file, metadata):
+    temp = SizeMtimeLinemode.infostring(self, file, metadata)
+    return temp
+
+@ranger.api.register_linemode
+class DannyPermissions(LinemodeBase):
+  name = "danny_perm"
+
+  def filetitle(self, file, metadata):
+    temp = devicon(file) + ' ' + file.relative_path
+    return temp
+  
+  def infostring(self, file, metadata):
+    temp = "%s %s %s" % (file.user, file.group, file.get_permission_string())
+    return temp
