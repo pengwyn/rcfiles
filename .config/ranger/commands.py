ba4b9be167a4ba37041228f473abd606455c505a
Daniel Cocks <daniel.cocks@gmail.com>
2019-09-27 10:09:21 +1000

    i3 and ranger

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -28,6 +28,30 @@ class fasd(Command):
             directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
             self.fm.cd(directory)
 
+class fasd_select(Command):
+    """
+    :fasd_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        command="fasd -d -l -r | fzf --height=50% --reverse --tac --no-sort --cycle"
+
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
 class fzf_select(Command):
     """
     :fzf_select
@@ -41,17 +65,14 @@ class fzf_select(Command):
     def execute(self):
         import subprocess
         import os.path
-        if False:
-            if self.quantifier:
-                # match only directories
-                command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-                -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-            else:
-                # match files and directories
-                command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-                -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
-            command="fasd -d -l -r | fzf --height=50% --reverse --tac --no-sort --cycle"
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
