6ad59faca6ea601ef0cb81ff4fe956133fad790f
Daniel Cocks <daniel.cocks@gmail.com>
2019-09-27 10:56:04 +1000

    i3

--- bin/i3_open_empty.py
+++ bin/i3_open_empty.py
@@ -7,4 +7,7 @@ workspaces = json.loads(output)
 
 next_num = next(i for i in range(1, 100) if not [ws for ws in workspaces if ws['num'] == i])
 
+import sys
+if len(sys.argv) == 2:
+    subprocess.call(['i3-msg', 'move container to workspace number %i' % next_num])
 subprocess.call(['i3-msg', 'workspace number %i' % next_num])
