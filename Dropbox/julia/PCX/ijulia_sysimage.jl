41e5c556d7cf63209dbbd2115f52a5c77351584c
Daniel Cocks <daniel.cocks@gmail.com>
2020-02-24 12:20:18 +1100

    Removed Blink and WebIO

--- Dropbox/julia/PCX/ijulia_sysimage.jl
+++ Dropbox/julia/PCX/ijulia_sysimage.jl
@@ -2,10 +2,10 @@ ENV["PLOTS_DEFAULT_BACKEND"] = ""
 
 using Revise, OhMyREPL, Plots, Debugger, DanUtils, Constants, MacroTools
 using IJulia
-using Interact, Blink, WebIO
+#using Interact, Blink, WebIO
 
 using PackageCompilerX
 
 create_sysimage([:Revise, :OhMyREPL, :Plots, :Debugger, :DanUtils, :Constants, :IJulia, :MacroTools,
-                 :Interact, :Blink, :WebIO
+                 #:Interact, :Blink, :WebIO
                  ], sysimage_path=expanduser("~/.julia/config/ijulia_sysimage.so"), precompile_statements_file="OMRPlotsDebugger_trace.jl", precompile_execution_file="precompile_plots.jl")
