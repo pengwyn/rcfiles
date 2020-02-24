417e48bb5d1a123ac32b7df6da783f2bf47a1bc3
Daniel Cocks <daniel.cocks@gmail.com>
2020-02-24 12:37:15 +1100

    Trying different sysimage

--- Dropbox/julia/PCX/ijulia_sysimage.jl
+++ Dropbox/julia/PCX/ijulia_sysimage.jl
@@ -1,11 +1,13 @@
-ENV["PLOTS_DEFAULT_BACKEND"] = ""
+ENV["PLOTS_DEFAULT_BACKEND"] = "gr"
 
-using Revise, OhMyREPL, Plots, Debugger, DanUtils, Constants, MacroTools
+using Revise, OhMyREPL, Debugger, DanUtils, Constants, MacroTools
+using Plots, GR
 using IJulia
 #using Interact, Blink, WebIO
 
 using PackageCompilerX
 
-create_sysimage([:Revise, :OhMyREPL, :Plots, :Debugger, :DanUtils, :Constants, :IJulia, :MacroTools,
+create_sysimage([:Revise, :OhMyREPL, :Debugger, :DanUtils, :Constants, :IJulia, :MacroTools,
+                 :Plots, :GR,
                  #:Interact, :Blink, :WebIO
                  ], sysimage_path=expanduser("~/.julia/config/ijulia_sysimage.so"), precompile_statements_file="OMRPlotsDebugger_trace.jl", precompile_execution_file="precompile_plots.jl")
