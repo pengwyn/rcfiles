8b6ff162fe97f5944b22683e9e80363f11435b2d
Daniel Cocks <daniel.cocks@gmail.com>
2020-02-26 16:10:10 +1100

    Adjusted stuff

--- bin/julia_PCX/ijulia_sysimage.jl
+++ bin/julia_PCX/ijulia_sysimage.jl
@@ -1,20 +1,29 @@
-ENV["PLOTS_DEFAULT_BACKEND"] = "gr"
+# ENV["PLOTS_DEFAULT_BACKEND"] = "unicodeplots"
 
-packages = [:Revise, :OhMyREPL, :Debugger, :IJulia, :MacroTools,
-                 #:Plots, :GR,
-                 #:Interact, :Blink, :WebIO,
+packages = [:Revise, :OhMyREPL, :Debugger, :MacroTools,
+            :IJulia,
+            #:Plots, :GR,
+            :PyCall,
+            # :UnicodePlots,
+            # :GR,
+            :Plots,
+            # :PyPlot,
+            #:Interact, :Blink, :WebIO,
+
 # All of my util packages
             :AndExport,
 			:AutoParameters,
 			:AutoNamedTuples,
-			:DanUtils, :Constants,
+			:Constants,
+            :DanUtils,
 			]
 
 
 for package in packages
-	@eval using $package
+	@eval import $package
 end
 
 using PackageCompiler
 
-create_sysimage(packages, sysimage_path=expanduser("~/.julia/config/ijulia_sysimage.so"), precompile_statements_file="OMR_trace.jl")#, precompile_execution_file="precompile_plots.jl")
+# create_sysimage(packages, sysimage_path=expanduser("~/.julia/config/ijulia_sysimage.so"), precompile_statements_file="OMR_trace.jl", precompile_execution_file="precompile_plots.jl")
+create_sysimage(packages, sysimage_path=expanduser("~/.julia/config/ijulia_sysimage.so"), precompile_statements_file="OMR_trace.jl")
