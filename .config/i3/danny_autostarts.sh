86bbac46c965253d7a2181abcc8b852a8f36842c
Daniel Cocks <daniel.cocks@gmail.com>
2020-03-09 09:38:48 +1100

    Made a proper wait now

--- .config/i3/danny_autostarts.sh
+++ .config/i3/danny_autostarts.sh
@@ -1,5 +1,29 @@
 #!/bin/zsh
 
+
+echo "Start" > /tmp/debugging
+function waitforwindow() {
+    # echo "Started waiting for $@ at $date" >> /tmp/debugging
+    start_time=$(date +%s)
+    while true ; do
+        sleep 0.1
+        if (( $(date +%s) - $start_time > 12 )) ; then
+            # Abort after 12 sec
+            break
+        fi
+
+        # "root" doesn't seem to work here.
+        # xwininfo -tree -root | grep $1
+        for id in $(xwininfo -tree -name "i3" | grep $@ | awk '{print $1}') ; do
+            xwininfo -id $id -shape | grep "IsViewable" > /dev/null
+            if [[ $? == 0 ]] ; then
+                # echo "Finished waiting at $date" >> /tmp/debugging
+                return
+            fi
+        done
+    done
+}
+
 # This is in a separate file to make it easier to put sleeps in etc...
 # pkill i3icons2 ; i3icons2
 
@@ -18,29 +42,32 @@ i3-msg 'exec evolution'
 i3-msg 'workspace number 10; move workspace to output primary'
 # chromium &!
 i3-msg 'exec chromium'
-sleep 1
+waitforwindow Chromium
 i3-msg 'move workspace to output right'
 
-i3-msg 'workspace number 8; move workspace to output primary'
-steam-native &!
+if [[ $(hostname) != "penganuix" ]] ; then
+    i3-msg 'workspace number 8; move workspace to output primary'
+    i3-msg 'exec steam-native'
+    waitforwindow steam
+fi
 
 # If there's no steam at all - break. Otherwise wait until the renderer starts up.
-sleep 2
-while true ; do
-    sleep 0.1
-    pgrep --full 'steamwebhelper' > /dev/null 2>&1
-    if [[ $? != 0 ]] ; then
-        echo "breakign because of steamwebhelper" > /tmp/debugging
-        break
-    fi
+# sleep 2
+# while true ; do
+#     sleep 0.1
+#     pgrep --full 'steamwebhelper' > /dev/null 2>&1
+#     if [[ $? != 0 ]] ; then
+#         echo "breakign because of steamwebhelper" > /tmp/debugging
+#         break
+#     fi
 
-    pgrep --full 'steamwebhelper.*renderer' > /dev/null 2>&1
-    if [[ $? == 0 ]] ; then
-        echo "breakign because of steamwebhelper and renderer" > /tmp/debugging
-        break
-    fi
-done
-sleep 1
+#     pgrep --full 'steamwebhelper.*renderer' > /dev/null 2>&1
+#     if [[ $? == 0 ]] ; then
+#         echo "breakign because of steamwebhelper and renderer" > /tmp/debugging
+#         break
+#     fi
+# done
+# sleep 1
 # i3-msg 'exec steam-native'
 
 
