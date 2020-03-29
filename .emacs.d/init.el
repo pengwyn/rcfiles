822c47dbe579de693f89622008cc9769c7a1fd49
Daniel Cocks <daniel.cocks@gmail.com>
2020-03-29 11:59:01 +1100

    Visit with line numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1844,6 +1844,23 @@ you want to quit windows on all frames."
     (when (= 0 (length (get-buffer-window-list (current-buffer) nil t)))
       (kill-buffer buf))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; * Visit files with line numbers
+;;--------------------------------
+(defadvice server-visit-files (before parse-numbers-in-lines (files proc &optional nowait) activate)
+  "looks for filenames like file:line or file:line:position and reparses name in such manner that position in file"
+  (ad-set-arg 0
+              (mapcar (lambda (fn)
+                        (let ((name (car fn)))
+                          (if (string-match "^\\(.*?\\):\\([0-9]+\\)\\(?::\\([0-9]+\\)\\)?$" name)
+                              (cons
+                               (match-string 1 name)
+                               (cons (string-to-number (match-string 2 name))
+                                     (string-to-number (or (match-string 3 name) "")))
+                               )
+                            fn))) files))
+  )
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; * Key bindings
 ;;----------------------------
