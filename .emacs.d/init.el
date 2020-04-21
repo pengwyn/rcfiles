a955f68bab617c29050fe27c5e2ea007502d1e9e
Daniel Cocks <daniel.cocks@gmail.com>
2020-04-22 09:43:59 +1000

    Cleaned up powerline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -422,7 +422,75 @@
 (use-package powerline
   :config
   (use-package powerline-evil)
-  (powerline-center-evil-theme))
+  (setq powerline-display-hud nil)
+  (setq powerline-display-buffer-size nil)
+  (setq powerline-gui-use-vcs-glyph t)
+
+  ;; (powerline-center-evil-theme)
+  ;; Based on powerline-center-evil-theme
+  (setq mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face0 (if active 'powerline-active0 'powerline-inactive0))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" face0 'l)
+                                     ;;
+                                     ;; (when powerline-display-buffer-size
+                                     ;;   (powerline-buffer-size face0 'l))
+                                     (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
+                                     (powerline-raw " " face0)
+                                     (funcall separator-left face0 face1)
+                                     (powerline-narrow face1 'l)
+                                     ;;
+                                     ;; (powerline-vc face1)
+                                     ))
+                          (rhs (list (powerline-raw global-mode-string face1 'r)
+                                     ;;
+                                     ;; (powerline-raw "%4l" face1 'r)
+                                     ;; (powerline-raw ":" face1)
+                                     ;; (powerline-raw "%3c" face1 'r)
+                                     (funcall separator-right face1 face0)
+                                     (powerline-raw " " face0)
+                                     (powerline-raw "%3p" face0 'r)
+                                     ;;
+                                     ;; (when powerline-display-hud
+                                     ;;   (powerline-hud face2 face1))
+                                     ))
+                          (center (append (list (powerline-raw " " face1)
+                                                (funcall separator-left face1 face2)
+                                                (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                                  (powerline-raw erc-modified-channels-object face2 'l))
+                                                (powerline-major-mode face2 'l)
+                                                (powerline-process face2)
+                                                (powerline-raw " " face2))
+                                          (if (split-string (format-mode-line minor-mode-alist))
+                                              (append (if evil-mode
+                                                          (list (funcall separator-right face2 face1)
+                                                                (powerline-raw evil-mode-line-tag face1 'l)
+                                                                (powerline-raw " " face1)
+                                                                (funcall separator-left face1 face2)))
+                                                      (list (powerline-minor-modes face2 'l)
+                                                            (powerline-raw " " face2)
+                                                            (funcall separator-right face2 face1)))
+                                            (list (powerline-raw evil-mode-line-tag face2)
+                                                  (funcall separator-right face2 face1))))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                             (powerline-render center)
+                             (powerline-fill face1 (powerline-width rhs))
+                             (powerline-render rhs)
+                             )))))
+  )
 
 (use-package magit
   :bind (("<f6>" . magit-status)
