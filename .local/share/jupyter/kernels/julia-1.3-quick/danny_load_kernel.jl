585170b9663ddabca161018919a5525121bcda7b
Daniel Cocks <daniel.cocks@gmail.com>
2020-05-20 11:54:06 +1000

    Remove some files

--- .local/share/jupyter/kernels/julia-1.3-quick/danny_load_kernel.jl
+++
@@ -1,4 +0,0 @@
-import IJulia
-
-ijulia_kernel_file = joinpath(dirname(pathof(IJulia)), "kernel.jl")
-include(ijulia_kernel_file)
--- compile_julia_packages.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck, :PyCall, :PyPlot)'
-#/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck, :PyCall)'
-#/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck)'
-#julia --startup-file=no -e 'using PackageCompiler; compile_package(:Plots, :DataStructures, :Roots, :Glob, :Dierckx, :DataFrames, :CSV, :SpecialFunctions, :Parameters, :ArgCheck)'
-
-# /usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:Revise, :OhMyREPL, :Constants, :DanUtils, :Plots, :DataStructures, :Dierckx, :DataFrames, :CSV, :SpecialFunctions)'
-# /usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:OhMyREPL, :Constants, :DanUtils, :Plots, :DataStructures, :Dierckx, :CSV, :SpecialFunctions, :Debugger)'
-/usr/bin/julia --startup-file=no -e 'using PackageCompiler; compile_incremental(:OhMyREPL, :Constants, :DanUtils, :Plots, :Debugger)'
